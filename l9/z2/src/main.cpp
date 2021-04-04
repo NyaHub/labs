d0a8a3f03a59cec08e6c3779b4975cc752029286
NyaHub <yaya70038@gmail.com>
2021-04-04 13:45:40 +0500

    l9 complete

---
+++ l9/z2/src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+float foo(int * arr, size_t len);
+void print_arr(int * arr, size_t len);
+
+int main(){
+	srand(time(0));
+
+	size_t mmmlen, wwwlen;
+
+	cout << "Enter len of mmm array: "; cin >> mmmlen;
+	cout << "Enter len of www array: "; cin >> wwwlen;
+
+	int mmm[mmmlen];
+	int www[wwwlen];
+	for(int i=0; i<max(mmmlen, wwwlen); i++){
+		if(i<wwwlen) www[i] = rand()%100+50;
+		if(i<mmmlen) mmm[i] = rand()%100+50;
+	}
+	cout << "mmm:" << endl;
+	print_arr(mmm, mmmlen);
+	cout << "www:" << endl;
+	print_arr(www, wwwlen);
+
+	cout << "AVG mmm: " << foo(mmm, mmmlen) << endl;
+	cout << "AVG www: " << foo(www, wwwlen) << endl;
+}
+
+float foo(int * arr, size_t len){
+	double out;
+	for(int i = 0; i<len; i++){
+		out += pow(arr[i], 2);
+	}
+	return sqrt(out);
+}
+
+void print_arr(int * arr, size_t len){
+	for(int i = 0; i < len; i++){
+		if(i%10 == 9) printf("%6i\n", arr[i]);
+		else printf("%6i", arr[i]);
+	}
+	cout << endl;
+}
