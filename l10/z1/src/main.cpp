1fb56356829908b9314d7c3cd2cda9987d72b0ed
NyaHub <yaya70038@gmail.com>
2021-04-06 20:59:43 +0500

    complete

--- l10/z1/src/main.cpp
+++ l10/z1/src/main.cpp
@@ -1,1 +1,25 @@
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <iostream>
 
+using namespace std;
+
+int main(){
+	ifstream infile("in.file");
+	if(!infile.good()){
+		cout<< "in file not found" << endl;
+		return 0;
+	}
+
+	ofstream outfile("out.file");
+
+	string line;
+	while(getline(infile, line)){
+		outfile << "'" << line << "'" << endl;
+	}
+	infile.close();
+	outfile.close();
+
+	return 0;
+}
