37358afb9f486a717fc477efec17b7d38ebb0aac
NyaHub <yaya70038@gmail.com>
2021-04-06 12:53:18 +0500

    tt

--- l9/z3/src/main.cpp
+++ l9/z3/src/main.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include <cmath>
 #include <ctime>
+#include <vector>
 
 using namespace std;
 
-float foo(int * arr, size_t len);
+vector<int> foo(int * a1, int * a2, size_t len);
 void print_arr(int * arr, size_t len);
+void print_arr(vector<int> arr, size_t len);
 
 int main(){
 	srand(time(0));
@@ -14,9 +16,12 @@ int main(){
 	size_t len;
 	cout << "Enter array length: "; cin >> len;
 
+	int pol[len];
+	int otr[len];
+
 	for(int i=0; i<len;i++){
-		pol[i]=rand()%100-200;
-		otr[i]=rand()%100-200;
+		pol[i]=rand()%100-50;
+		otr[i]=rand()%100-50;
 	}
 
 	cout<<"POL:"<<endl;
@@ -24,24 +29,32 @@ int main(){
 	cout<<"OTR:"<<endl;
 	print_arr(otr, len);
 
-	s = foo(pol, otr, len);
+	auto s = foo(pol, otr, len);
 
 	cout<<"S:"<<endl;
-	print_arr(s, len);
+	print_arr(s, s.size());
 
 	return 0;
 }
 
-float foo(int * a1, int * a2, size_t len){
+
+vector<int> foo(int * a1, int * a2, size_t len){
 	vector<int> tmp;
 	for(int i = 0; i<len; i++){
 		if (a1[i]>0)tmp.push_back(a1[i]);
-		if (a2[i]>0)tmp.push_back(a2[i]);
+		if (a2[i]<0)tmp.push_back(a2[i]);
 	}
 	return tmp;
 }
 
 void print_arr(int * arr, size_t len){
+	for(int i = 0; i < len; i++){
+		if(i%10 == 9) printf("%6i\n", arr[i]);
+		else printf("%6i", arr[i]);
+	}
+	cout << endl;
+}
+void print_arr(vector<int> arr, size_t len){
 	for(int i = 0; i < len; i++){
 		if(i%10 == 9) printf("%6i\n", arr[i]);
 		else printf("%6i", arr[i]);
